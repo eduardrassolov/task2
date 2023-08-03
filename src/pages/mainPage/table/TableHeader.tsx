c3ad0369f9ec45dae3d841828d892747173ed053
eduardrassolov <eduard.rassolov@outlook.com>
2023-08-03 08:01:54 +0300

    adde logic archive/unarchive notes

--- src/pages/mainPage/table/TableHeader.tsx
+++ src/pages/mainPage/table/TableHeader.tsx
@@ -1,15 +1,32 @@
-import { ArchiveBoxArrowDownIcon, TrashIcon } from "@heroicons/react/24/solid";
+import {
+  ArchiveBoxArrowDownIcon,
+  TrashIcon,
+  ArchiveBoxXMarkIcon,
+} from "@heroicons/react/24/solid";
 import { IHeaders } from "../../../interfaces/IHeaders";
 import store from "../../../redux/store";
+import { useState } from "react";
 
 type TableHeaderProps = {
   headers: Array<IHeaders>;
 };
 
 export default function TableHeader({ headers }: TableHeaderProps) {
+  const [toggleArchive, setToggle] = useState(true);
+
   const handleDeleteAll = () =>
     store.dispatch({ type: "notes/deleteAllNotes" });
+  const handleArhiveAll = () => {
+    setToggle((prev) => !prev);
 
+    console.log(toggleArchive);
+    store.dispatch({
+      type: "notes/archiveAllNotes",
+      payload: toggleArchive,
+    });
+  };
+
+  console.log(store.getState());
   return (
     <thead className="bg-gray-300 text-xl font-semibold text-left">
       <tr>
@@ -23,10 +40,14 @@ export default function TableHeader({ headers }: TableHeaderProps) {
             <button>
               <ArchiveBoxArrowDownIcon className="w-6 opacity-0 cursor-auto" />
             </button>
-            <button>
-              <ArchiveBoxArrowDownIcon className="w-6" />
+            <button onClick={handleArhiveAll} className="hover:scale-[1.1]">
+              {toggleArchive ? (
+                <ArchiveBoxArrowDownIcon className="w-6" />
+              ) : (
+                <ArchiveBoxXMarkIcon className="w-6" />
+              )}
             </button>
-            <button onClick={handleDeleteAll}>
+            <button onClick={handleDeleteAll} className="hover:scale-[1.1]">
               <TrashIcon className="w-6" />
             </button>
           </div>
